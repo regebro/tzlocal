8d50ba8cf6e1b06e9e40c106711156cc2d21f1f9
BenjamenMeyer <ben.meyer@rackspace.com>
2014-01-10 11:18:10 -0600

    Added functionality to retrieve either the Windows Zone name or the Olsen Zone name from a TimeZone entry returned by tzlocal.get_localzone()

--- tzlocal/__init__.py
+++ tzlocal/__init__.py
@@ -5,4 +5,5 @@ elif 'darwin' in sys.platform:
     from tzlocal.darwin import get_localzone, reload_localzone
 else:
     from tzlocal.unix import get_localzone, reload_localzone
-    
+
+from tzlocal.olsen import get_windowszone_name, get_olsenzone_name
