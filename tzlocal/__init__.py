8cb8b3261e7bb45f011672bc6f0db3c3aa654128
Benjamin Bach <benjamin@overtag.dk>
2017-10-20 18:03:31 +0200

    If we have the magic string 'local' then no timezone was detected #44

--- tzlocal/__init__.py
+++ tzlocal/__init__.py
@@ -1,16 +1,17 @@
 import sys
 
-
-_cache_tz = None
-
-
 __version__ = "1.5dev0"
 
+# Cache variable
+_cache_tz = None
 
 def get_localzone():
     """Returns the zoneinfo-based tzinfo object that matches the local system's timezone."""
     global _cache_tz
-    
+
+    if _cache_tz is not None:
+        return _cache_tz
+
     # Avoid importing sub-modules at load time, breaks running setup.py
     if sys.platform == 'win32':
         from tzlocal.win32 import _get_localzone
@@ -19,8 +20,10 @@ def get_localzone():
     else:
         from tzlocal.unix import _get_localzone
 
-    if _cache_tz is None:
-        _cache_tz = _get_localzone()
+    if _cache_tz == 'local':
+        import pytz
+        raise pytz.UnknownTimeZoneError("Got timezone 'local', this is not a valid local timezone code")
+
     return _cache_tz
 
 
